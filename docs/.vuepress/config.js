a1528167bcd3b4bf9969bdcd6ae9cb15198ac1d9
Noel Mace <contact@noelmace.com>
2020-05-20 12:30:49 +0200

    feat(docs): add fr learning material

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,130 +1,155 @@
+const { sidebar: materialSidebarFr } = require("vuepress-bar")(
+  `${__dirname}/../fr/material/`,
+  {
+    addReadMeToFirstGroup: false,
+    stripNumbers: true,
+  }
+);
+
 module.exports = {
   locales: {
-    '/': {
-      lang: 'en-US',
-      title: 'FullWeb.dev',
-      description: 'A transverse vision of modern web development',
+    "/": {
+      lang: "en-US",
+      title: "FullWeb.dev",
+      description: "A transverse vision of modern web development",
     },
-    '/fr/': {
-      lang: 'fr-FR',
-      title: 'FullWeb.dev',
-      description: 'Une approche transversale du développement Web',
-    }
+    "/fr/": {
+      lang: "fr-FR",
+      title: "FullWeb.dev",
+      description: "Une approche transversale du développement Web",
+    },
   },
   themeConfig: {
-    logo: '/favicon/icon-384x384.png',
+    logo: "/favicon/icon-384x384.png",
     displayAllHeaders: false,
     sidebarDepth: 2,
-    repo: 'fullwebdev/fullwebdev',
-    docsDir: 'docs',
+    repo: "fullwebdev/fullwebdev",
+    docsDir: "docs",
     editLinks: true,
     locales: {
-      '/': {
+      "/": {
         // text for the language dropdown
-        selectText: 'Languages',
+        selectText: "Languages",
         // label for this locale in the language dropdown
-        label: 'English',
+        label: "English",
         // Aria Label for locale in the dropdown
-        ariaLabel: 'Languages',
+        ariaLabel: "Languages",
         // text for the edit-on-github link
-        editLinkText: 'Edit this page on GitHub',
+        editLinkText: "Edit this page on GitHub",
         // config for Service Worker
         serviceWorker: {
           updatePopup: {
             message: "New content is available.",
-            buttonText: "Refresh"
-          }
+            buttonText: "Refresh",
+          },
         },
         // algolia docsearch options for current locale
         algolia: {},
         nav: [
-          { text: 'Home', link: '/' },
-          { text: 'About', link: '/about/' },
+          { text: "Home", link: "/" },
+          { text: "About", link: "/about/" },
         ],
-        sidebar: [
-          '/',
-          '/introduction/',
-          '/conferences/',
-          '/codelabs/',
-          '/flashcards/'
-        ],
-        lastUpdated: 'Last Updated'
+        sidebar: {
+          "/": [
+            "/",
+            "/introduction/",
+            "/conferences/",
+            "/codelabs/",
+            "/flashcards/",
+          ],
+        },
+        lastUpdated: "Last Updated",
       },
-      '/fr/': {
+      "/fr/": {
         // text for the language dropdown
-        selectText: 'Langues',
+        selectText: "Langues",
         // label for this locale in the language dropdown
-        label: 'Français',
+        label: "Français",
         // Aria Label for locale in the dropdown
-        ariaLabel: 'Langues',
+        ariaLabel: "Langues",
         // text for the edit-on-github link
-        editLinkText: 'Éditez cette page sur GitHub',
+        editLinkText: "Éditez cette page sur GitHub",
         // config for Service Worker
         serviceWorker: {
           updatePopup: {
             message: "Une mise à jour est disponible.",
-            buttonText: "Recharger"
-          }
+            buttonText: "Recharger",
+          },
         },
         // algolia docsearch options for current locale
         algolia: {},
         nav: [
-          { text: 'Accueil', link: '/fr/' },
-          { text: 'À propos', link: '/fr/about/' },
+          { text: "Accueil", link: "/fr/" },
+          { text: "À propos", link: "/fr/about/" },
         ],
-        sidebar: [
-          ['/fr/', 'Accueil'],
-          '/fr/introduction/',
-          '/fr/conferences/',
-          '/fr/codelabs/',
-          '/fr/flashcards/'
-        ],
-        lastUpdated: 'Dernière mise à jour'
-      }
-    }
+        sidebar: {
+          "/fr/material/": materialSidebarFr,
+          "/": [
+            ["/fr/", "Accueil"],
+            "/fr/introduction/",
+            "/fr/conferences/",
+            "/fr/codelabs/",
+            "/fr/flashcards/",
+            "/fr/material/",
+          ],
+        },
+        lastUpdated: "Dernière mise à jour",
+      },
+    },
   },
-  dest: 'dist',
+  dest: "dist",
   plugins: [
-    '@vuepress/google-analytics',
+    "@vuepress/google-analytics",
     [
-      '@vuepress/pwa',
+      "@vuepress/pwa",
       {
         serviceWorker: true,
         updatePopup: false,
         generateSWConfig: {
-          swDest: 'dist/service-worker.js',
+          swDest: "dist/service-worker.js",
           skipWaiting: true,
           clientsClaim: true,
-          globDirectory: 'dist',
-          globPatterns: ['**/*.{html,js,css,png,gif}'],
+          globDirectory: "dist",
+          globPatterns: ["**/*.{html,js,css,png,gif}"],
         },
       },
     ],
   ],
-  ga: 'G-681S65029Y',
+  ga: "G-681S65029Y",
   head: [
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['link', { rel: 'icon', type: 'image/png', href: '/favicon/favicon-96x96.png' }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
     [
-      'meta',
+      "link",
+      { rel: "icon", type: "image/png", href: "/favicon/favicon-96x96.png" },
+    ],
+    [
+      "meta",
       {
-        property: 'og:image',
+        property: "og:image",
         content:
-          'https://raw.githubusercontent.com/fullwebdev/fullwebdev/master/docs/.vuepress/public/favicon/icon-384x384.png',
+          "https://raw.githubusercontent.com/fullwebdev/fullwebdev/master/docs/.vuepress/public/favicon/icon-384x384.png",
       },
     ],
-    ['meta', { property: 'og:site_name', content: 'FullWeb.dev' }],
+    ["meta", { property: "og:site_name", content: "FullWeb.dev" }],
     [
-      'meta',
+      "meta",
       {
-        property: 'og:description',
-        content:
-          'Modern Web Architecture & Development',
+        property: "og:description",
+        content: "Modern Web Architecture & Development",
       },
     ],
-    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
-    ['meta', { name: 'twitter:image', content: 'https://fullweb.dev/social.png' }],
-    ['meta', { name: 'twitter:image:alt', content: 'A transverse vision of modern web development' }],
-    ['meta', { name: 'twitter:title', content: 'FullWeb.dev' }],
+    ["meta", { name: "twitter:card", content: "summary_large_image" }],
+    [
+      "meta",
+      { name: "twitter:image", content: "https://fullweb.dev/social.png" },
+    ],
+    [
+      "meta",
+      {
+        name: "twitter:image:alt",
+        content: "A transverse vision of modern web development",
+      },
+    ],
+    ["meta", { name: "twitter:title", content: "FullWeb.dev" }],
   ],
 };
