eb4f937dc387e35ad6a4e3d9b93b73d1f23f454f
Noel Mace <contact@noelmace.com>
2020-06-06 17:35:08 +0200

    refactor(code-samples): examples for emoji CE

--- packages/code-samples/src/components/custom-elements/names/index.js
+++ packages/code-samples/src/components/custom-elements/names/index.js
@@ -41,9 +41,40 @@ const isValidElementName = (str) => {
  * @param {string} str custom element name to validate
  */
 const isValidCustomElementName = (str) => {
-  const PCEnChar =
-    "-|.|[0-9]|_|[a-z]|\u{B7}|[\u{C0}-\u{D6}]|[\u{D8}-\u{F6}]|[\u{F8}-\u{37D}]|[\u{37F}-\u{1FFF}]|[\u{200C}-\u{200D}]|[\u{203F}-\u{2040}]|[\u{2070}-\u{218F}]|[\u{2C00}-\u{2FEF}]|[\u{3001}-\u{D7FF}]|[\u{F900}-\u{FDCF}]|[\u{FDF0}-\u{FFFD}]|[\u{10000}-\u{EFFFF}]";
+  //#region PotentialCustomElementName
+  const PCEnChar = [
+    /* Basin Latin + middle dot
+     Exclude:
+     ! " # $ % & ' ( ) * + , / : ;
+     < = > ? @ [ \ ] ^ ` { | } ~
+     ¡ ¢ £ ¤ ¥ ¦ § ¨ © ª « ¬ ® ¯
+     ° ± ² ³ ´ µ ¶ */
+    "-|.|[0-9]|_|[a-z]|·",
+    // Latin 1 Letters
+    // exclude multiplication division signs
+    "[À-Ö]|[Ø-ö]|[ø-ÿ]",
+    // LatinExtended to Combining Diacritical Marks
+    "[\u{100}-\u{36F}]",
+    // Greek & Coptic Letters
+    "[\u{370}-\u{37D}]|[\u{37F}-\u{3FF}]",
+    // Cyrillic to Greek Extended
+    "[\u{400}-\u{1FFF}]",
+    // zero width
+    "[\u{200C}-\u{200D}]",
+    // tie
+    "[\u{203F}-\u{2040}]",
+    // Superscripts to Number Forms
+    "[\u{2070}-\u{218F}]",
+    // Glagolitic to Kangxi Radicals
+    "[\u{2C00}-\u{2FEF}]",
+    "[\u{3001}-\u{D7FF}]",
+    "[\u{F900}-\u{FDCF}]",
+    "[\u{FDF0}-\u{FFFD}]",
+    "[\u{10000}-\u{EFFFF}]",
+  ].join("|");
+
   const PotentialCustomElementName = `[a-z](${PCEnChar})*-(${PCEnChar})*`;
+  //#endregion PotentialCustomElementName
 
   return new RegExp(PotentialCustomElementName, "u").test(
     str
