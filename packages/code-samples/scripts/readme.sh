faf99711fcda9bddaa32b1b06e4d31d809baa8f4
Noel Mace <contact@noelmace.com>
2020-06-11 08:43:07 +0200

    build(code-samples): watch README

--- packages/code-samples/scripts/readme.sh
+++ packages/code-samples/scripts/readme.sh
@@ -1,7 +1,9 @@
+# TODO: move to vuepress
+
 WARNING="<!--
 
   !!! DO NOT EDIT !!!
-  
+
   THIS FILE WAS AUTO-GENERATED
 
   Changes to this file can be made in README.md
@@ -9,8 +11,20 @@ WARNING="<!--
 -->
 "
 
-HTML=$(pandoc -s src/README.md -t html --metadata title="Modern Web Apps - Demos")
+function build_index() {
+  HTML=$(pandoc -s src/README.md -t html --metadata title="Modern Web Apps - Demos")
 
-echo -e "${WARNING}${HTML}" > src/index.html
+  echo -e "${WARNING}${HTML}" > src/index.html
+}
 
-git add src/index.html
+build_index
+
+if [ "$1" = "--watch" ]; then
+  echo "watching src/README.md..."
+
+  # requires https://github.com/inotify-tools/inotify-tools/wiki
+  while inotifywait -e modify src/README.md; do
+    build_index
+    echo "new version"
+  done
+fi;
