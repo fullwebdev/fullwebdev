4294153edce1010b20a8d0827c04a73830e140a5
Noel Mace <contact@noelmace.com>
2020-07-22 15:32:58 +0200

    build: directly extract chapters w/o parts in livre-fr

--- scripts/extract-material.js
+++ scripts/extract-material.js
@@ -3,12 +3,12 @@
 const fs = require("fs-extra");
 const path = require("path");
 
-const codeSampleRe = /^<<< @\/\.\.\/code-samples\/(([^#]*)(?:#.*)?)$/;
+const codeSampleRe = /^<<< @\/\.\.\/([\w-]+)\/(([^#]*)(?:#.*)?)$/;
 
 //const lineRe = /^(<<< @|#|!\[)/;
 const lineRe = /^(<<< @|## |!\[|<!--en:)/;
 
-const ignoredDir = [".vuepress", "refs", "0-introduction"];
+const ignoredDir = [".vuepress", "refs", "pages"];
 
 function filterFile() {
   const srcRoot = path.join(__dirname, "..", "packages", "livre-fr", "src");
@@ -27,7 +27,7 @@ function filterFile() {
     });
   }
 
-  const parts = fs
+  const chapters = fs
     .readdirSync(srcRoot)
     .filter(
       (file) =>
@@ -35,76 +35,77 @@ function filterFile() {
         !ignoredDir.includes(path.basename(file))
     );
 
-  parts.forEach((partName) => {
-    const part = path.join(srcRoot, partName);
-    const chapters = fs
-      .readdirSync(part)
-      .filter((file) => fs.lstatSync(path.join(part, file)).isDirectory());
+  console.log(chapters);
 
-    chapters.forEach((chapterName) => {
-      const chapter = path.join(part, chapterName);
-      const sources = fs
-        .readdirSync(chapter)
-        .filter((file) => file !== "TITLE.md");
+  chapters.forEach((chapterName) => {
+    const chapter = path.join(srcRoot, chapterName);
+    const sources = fs
+      .readdirSync(chapter)
+      .filter((file) => file !== "TITLE.md");
 
-      sources.forEach((sourceName) => {
-        const source = path.join(chapter, sourceName);
-        const text = fs.readFileSync(source, { encoding: "utf8" });
+    console.log(sources);
 
-        const rslt = {
-          fr: [],
-          en: [],
-        };
+    sources.forEach((sourceName) => {
+      const source = path.join(chapter, sourceName);
+      const text = fs.readFileSync(source, { encoding: "utf8" });
 
-        const materialCount = {
-          fr: 0,
-          en: 0,
-        };
+      const rslt = {
+        fr: [],
+        en: [],
+      };
 
-        for (const line of text.split("\n")) {
-          if (line.match(lineRe)) {
-            let rsltLine = {
-              fr: "",
-              en: "",
-            };
+      const materialCount = {
+        fr: 0,
+        en: 0,
+      };
 
-            if (line.startsWith("<<< ")) {
-              const [full, snippetPath, filePath] = codeSampleRe.exec(line);
-              rsltLine.en = rsltLine.fr = `_[${filePath}](https://github.com/fullwebdev/fullwebdev/tree/master/packages/code-samples/${filePath}):_
+      for (const line of text.split("\n")) {
+        if (line.match(lineRe)) {
+          let rsltLine = {
+            fr: "",
+            en: "",
+          };
 
-<<< @/../packages/code-samples/${snippetPath}`;
-              materialCount.fr++;
-              materialCount.en++;
-            } else if (line.startsWith("![")) {
-              // TODO: add link to image source on GitHub
-              rsltLine.fr = line;
-              materialCount.fr++;
-            } else if (line.startsWith("##")) {
-              rsltLine.fr = line.slice(1);
-            } else if (line.startsWith("<!--en: ")) {
-              const [full, content] = /^<!--en: (.*)-->$/.exec(line);
-              rsltLine.en = `# ${content}`;
-            }
+          if (line.startsWith("<<< ")) {
+            const [full, package, snippetPath, filePath] = codeSampleRe.exec(
+              line
+            );
+            // FIXME: path to submodules
+            rsltLine.en = rsltLine.fr = `_[${filePath}](https://github.com/fullwebdev/fullwebdev/tree/master/packages/${package}/${filePath}):_
 
-            for (const lang in rslt) {
-              if (rsltLine[lang]) {
-                rslt[lang].push(rsltLine[lang]);
-              }
+<<< @/../packages/${package}/${snippetPath}`;
+            materialCount.fr++;
+            materialCount.en++;
+          } else if (line.startsWith("![")) {
+            // TODO: add link to image source on GitHub
+            rsltLine.fr = line;
+            materialCount.fr++;
+          } else if (line.startsWith("##")) {
+            rsltLine.fr = line.slice(1);
+          } else if (line.startsWith("<!--en: ")) {
+            const [full, content] = /^<!--en: (.*)-->$/.exec(line);
+            rsltLine.en = `# ${content}`;
+          }
+
+          for (const lang in rslt) {
+            if (rsltLine[lang]) {
+              rslt[lang].push(rsltLine[lang]);
             }
           }
         }
+      }
 
-        for (const lang in dest) {
-          if (materialCount[lang] > 0) {
-            const relativePath = path.relative(srcRoot, source);
-            const destFile = path.join(dest[lang], relativePath);
-            fs.ensureFileSync(destFile);
-            fs.writeFileSync(destFile, rslt[lang].join("\n\n").concat("\n"), {
-              encoding: "utf8",
-            });
-          }
+      for (const lang in dest) {
+        if (materialCount[lang] > 0) {
+          const relativePath = path.relative(srcRoot, source);
+          const destFile = path.join(dest[lang], relativePath);
+          console.log(destFile);
+          fs.ensureFileSync(destFile);
+          fs.writeFileSync(destFile, rslt[lang].join("\n\n").concat("\n"), {
+            encoding: "utf8",
+          });
         }
-      });
+      }
     });
   });
 }
