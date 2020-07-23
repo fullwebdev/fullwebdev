9b79bd7efe6250957125e3582df01104c8b16402
Noel Mace <contact@noelmace.com>
2020-07-23 23:46:17 +0200

    feat(code-samples): explain rendering performance

---
+++ packages/code-samples/src/rendering/limitations/dom-api/heavy-rendering/create-content.js
@@ -0,0 +1,12 @@
+import { heavyHTML } from "./heavy-html.js";
+
+function render() {
+  //#region render
+  const container = document.createElement("div");
+  document.querySelector("main").appendChild(container);
+  container.innerHTML = heavyHTML();
+  // container.innerHTML = '<p>Hello World</p>'
+  //#endregion render
+}
+
+window.render = render;
