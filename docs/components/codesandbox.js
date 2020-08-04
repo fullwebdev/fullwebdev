806940dde6e2dbaf11a107033409ceea77b6fc8d
Noel Mace <contact@noelmace.com>
2020-08-04 12:25:57 +0200

    chore(docs): better codesandbox style

--- docs/components/codesandbox.js
+++ docs/components/codesandbox.js
@@ -5,7 +5,7 @@ import { html } from "lit-html";
  */
 export default (data) => html`<iframe
   src="https://codesandbox.io/embed/github/fullwebdev/fullwebdev/tree/${data.branch}/${data.path}?fontsize=14&module=%2FREADME.md&theme=light"
-  style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+  style="width:100%; height:80vh; border:1px solid #eaecef; border-radius: 4px; overflow:hidden;"
   title="${data.title}"
   sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
 ></iframe>`;
