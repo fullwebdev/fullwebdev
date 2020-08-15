adc968dcc3705a1a2dfda94478a387ce70a4514e
Noel Mace <contact@noelmace.com>
2020-08-15 19:09:57 +0200

    chore(docs): home "features"

--- docs/pages/en/index.js
+++ docs/pages/en/index.js
@@ -2,10 +2,26 @@ import home from "../../js/components/home.js";
 
 export default () =>
   home({
-    heroText: "Modern Web",
-    heroSubText: "Architecture & Development",
-    actionText: "Get Started",
-    features: [],
+    heroText: "Full Web",
+    heroSubText: "Development",
+    actionText: "Let's go",
     footer: "Copyright © 2020 Noël Macé",
-    tagline: "A transverse vision of modern web development",
+    tagline: "Mastering the whole Modern Web",
+    features: [
+      {
+        title: "Accessible",
+        details:
+          "Our first goal is to help everyone master modern web development. This requires taking action against discriminations. Following the principles of web accessibility and making our creations as understandable as possible is only the first step.",
+      },
+      {
+        title: "Future-proof",
+        details:
+          "FullWeb.dev is against all dogmatism. We strive for long term substainability of webdev skills and projects. We strongly believe that focusing on the web platform itself and understanding what exactly makes each tool's strengths and weaknesses is the best way to achieve this goal.",
+      },
+      {
+        title: "Open & Fair",
+        details:
+          "Most of our creations are Open-Source, in order to facilitate contributions. However, some of our content may require copyright or paywall. When this is the case, we do our best to never discriminate against anyone.",
+      },
+    ],
   });
--- docs/pages/fr/index.js
+++ docs/pages/fr/index.js
@@ -2,10 +2,26 @@ import home from "../../js/components/home.js";
 
 export default () =>
   home({
-    heroText: "Architecture & Développement",
-    heroSubText: "pour le Web moderne",
+    heroText: "Full Web",
+    heroSubText: "Development",
     actionText: "Allons-y !",
-    features: [],
     footer: "Copyright © 2020 Noël Macé",
-    tagline: "Une approche transversale du développement Web",
+    tagline: "Maitriser l'ensemble du Web moderne",
+    features: [
+      {
+        title: "Accessible",
+        details:
+          "Notre objectif numéro 1 : abattre les obstacles placés entre vous et la maîtrise du développement Web. Eviter et combattre toute discrimination a ainsi pour nous une grande importance. Ce pourquoi nous mettons notamment en oeuvre toute pratique d'accessibilité du Web nécessaire, et nous efforçons de rendre nos créations les plus intelligibles possible.",
+      },
+      {
+        title: "Prêt pour le futur",
+        details:
+          "FullWeb.dev est non dogmatique. Se focaliser sur les éléments qui constituent la part commune du développement Web, et étudier les forces et faiblesses de chaque solution en fonction du contexte plutôt que de manière absolue, est, selon nous, le meilleur moyen de garantir la pérennité de vos compétences et projets.",
+      },
+      {
+        title: "Ouvert & Équitable",
+        details:
+          "Nous partageons l'essentiel de nos création en Open Source avec la communauté, afin de permettre à tous de contribuer à leur amélioration. Dans le cas où une certaine part de notre travail nécessite un accès limité pour une quelconque raison, nous faisons tout notre possible pour garantir que celui-ci ne se fasse au détriment de personne.",
+      },
+    ],
   });
