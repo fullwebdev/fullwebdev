c9ca6624ae978ffec5f0309874da8de8a6542fba
Noel Mace <contact@noelmace.com>
2020-08-21 13:26:05 +0200

    fix(wof-slides): code samples on medium screen

--- src/assets/code-samples/app-gap/share-target-handling.js
+++ src/assets/code-samples/app-gap/share-target-handling.js
@@ -1,9 +1,8 @@
-// ?title=hello&text=world
+// ?title=hello
 
 window.addEventListener('DOMContentLoaded', () => {
     const parsedUrl = new URL(window.location);
-    // searchParams.get() will properly handle decoding the values.
+
     console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
-    console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
     console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
 });
