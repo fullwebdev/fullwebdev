f2a3f4aa7f0a15f17a9fdbf42077b6f0fb717fcd
Noel Mace <contact@noelmace.com>
2020-08-21 13:23:26 +0200

    feat(wof-slides): web share target api - code samples

--- src/assets/code-samples/app-gap/share-target-handling.js
+++ src/assets/code-samples/app-gap/share-target-handling.js
@@ -1,8 +1,9 @@
-// ?title=hello
+// ?title=hello&text=world
 
 window.addEventListener('DOMContentLoaded', () => {
     const parsedUrl = new URL(window.location);
-
+    // searchParams.get() will properly handle decoding the values.
     console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
+    console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
     console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
 });
