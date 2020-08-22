d4c2cf17d785f038bfcc16cac27c185af453b92a
Noel Mace <contact@noelmace.com>
2020-08-22 03:11:53 +0200

    feat(slides-vanilla): react & WC code samples

--- src/assets/code-samples/wc-in-react.js
+++ src/assets/code-samples/wc-in-react.js
@@ -1,9 +1,5 @@
 class HelloMessage extends React.Component {
-    render() {
-        return (
-            <div>
-                Bonjour <x-search>{this.props.name}</x-search> !
-            </div>
-        );
-    }
+  render() {
+    return <div>Bonjour <x-search>{this.props.name}</x-search> !</div>;
+  }
 }
