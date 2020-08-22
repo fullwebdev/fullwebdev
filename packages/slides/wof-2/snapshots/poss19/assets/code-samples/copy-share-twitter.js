49399c4c41e2ec607744f39bef61d2cecaa634e4
Noel Mace <contact@noelmace.com>
2020-08-22 21:05:55 +0200

    fix(slides-wof-2): update poss19 snapshot

--- packages/slides/wof-2/snapshots/poss19/assets/code-samples/copy-share-twitter.js
+++ packages/slides/wof-2/snapshots/poss19/assets/code-samples/copy-share-twitter.js
@@ -1,8 +1,8 @@
 const tweets = {
-    slides: `Hey look, I can share my slides with the Twitter #PWA using the Web Share & Web Share Target APIs! 🙌 #usetheplatform #WebOnFIRE wof.show`,
-    camera: `I can share a picture with the Twitter #PWA from the Camera App using the Web Share Target API Level 2 😁! #WebOnFIRE #usetheplatform wof.show`,
-    picture: `I can also share a picture from my own app using the Web Share & Web Share Target API Level 2! #WebOnFIRE #usetheplatform wof.show`,
-    clipboardPic: `And I can even share an image using the Async Clipboard Level 2 🤯! #WebOnFIRE #usetheplatform wof.show`
+    slides: `Hey look, I can share my slides with the Twitter #PWA using the Web Share & Web Share Target APIs! 🙌 #usetheplatform #WebOnFIRE`,
+    camera: `I can share a picture with the Twitter #PWA from the Camera App using the Web Share Target API Level 2 😁! #WebOnFIRE #usetheplatform`,
+    picture: `I can also share a picture from my own app using the Web Share & Web Share Target API Level 2! #WebOnFIRE #usetheplatform`,
+    clipboardPic: `And I can even share an image using the Async Clipboard Level 2 🤯! #WebOnFIRE #usetheplatform`
 };
 
 const getImgFile = async (url) => {
@@ -40,7 +40,7 @@ const share = async (data) => {
 document.querySelector('.share-btn-slides').addEventListener('click', () =>
     share({
         text: tweets.slides,
-        url: 'https://devoxxbe-2019.wof.show'
+        url: 'https://fullweb.dev/slides/wof/poss19/'
     })
 );
 
