ec0cdfece472514c281197af93f9adf13230a754
Noel Mace <contact@noelmace.com>
2020-08-24 16:10:28 +0200

    feat(panpress-cli): allow route aliases without numbering
    removes [0-9]+- prefix for keys of children routes in routes.js
    using this file, the runtime router can now find the js file to import without
    needing to iterate over all the routes if it doesn't have its exact name

--- packages/panpress/cli/src/generate/dir-to-routes.js
+++ packages/panpress/cli/src/generate/dir-to-routes.js
@@ -1,3 +1,8 @@
+const rmFilePrefix = (str) => {
+  const parsed = /^[0-9]+-(.*)$/.exec(str);
+  return parsed ? parsed[1] : str;
+};
+
 /**
  * @param {import('../utils/tree-node').DirTree} dirTree
  * @param {(path?: string) => Promise<{ [key: string ] : any} | null>} [asyncViewData]
@@ -16,13 +21,15 @@ async function dirToRoutes(dirTree, asyncViewData) {
     }
 
     if (root["index.js"]) {
-      menu.path = "/" + pathStack.join("/") + "/";
+      menu.path = "/" + pathStack.map(rmFilePrefix).join("/") + "/";
+      menu.realPath = "/" + pathStack.join("/") + "/";
+      menu.file = `/views${menu.realPath}index.js`;
     }
 
     for (let [name, node] of Object.entries(root)) {
       if (name === "index.js") {
         if (asyncViewData && menu.path) {
-          const data = await asyncViewData(menu.path);
+          const data = await asyncViewData(menu.realPath);
           Object.assign(menu, data);
         }
       } else {
@@ -36,20 +43,26 @@ async function dirToRoutes(dirTree, asyncViewData) {
           }
         }
 
-        const potentielPath = "/" + [...pathStack, name].join("/");
-        let key = name;
+        const realPathElms = [...pathStack, name];
+        const pathElms = realPathElms.map(rmFilePrefix);
+
+        const potentialPath = "/" + pathElms.join("/");
+        const potentialRealPath = "/" + realPathElms.join("/");
+        let key = pathElms.slice(-1)[0];
 
         if (node["index.js"]) {
-          entry.path = potentielPath.concat("/");
-          entry.file = `/views${entry.path}index.js`;
+          entry.path = potentialPath.concat("/");
+          entry.realPath = potentialRealPath.concat("/");
+          entry.file = `/views${entry.realPath}index.js`;
         } else if (name.substr(-3) === ".js") {
-          entry.file = `/views${potentielPath}`;
-          entry.path = potentielPath.slice(0, -3);
+          entry.file = `/views${potentialRealPath}`;
+          entry.path = potentialPath.slice(0, -3);
+          entry.realPath = potentialRealPath.slice(0, -3);
           key = entry.path.split("/").pop();
         }
 
         if (asyncViewData && entry.path) {
-          const data = await asyncViewData(entry.path);
+          const data = await asyncViewData(entry.realPath);
           if (data === null) {
             entry = null;
           } else {
