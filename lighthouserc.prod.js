c6ed66133984001af4cde25db748eb398814d1b7
Noel Mace <contact@noelmace.com>
2020-08-26 19:56:38 +0200

    ci: fix lighthouse prod config

--- lighthouserc.prod.js
+++ lighthouserc.prod.js
@@ -1,12 +1,18 @@
 const baseConfig = require("./lighthouserc.base");
 const merge = require("deepmerge");
 
-module.exports = merge(baseConfig, {
-  ci: {
-    collect: {
-      url: baseConfig.ci.collect.url.map((url) =>
-        url.replace("http://localhost", "https://fullweb.dev")
-      ),
+module.exports = merge(
+  baseConfig,
+  {
+    ci: {
+      collect: {
+        url: baseConfig.ci.collect.url.map((url) =>
+          url.replace("http://localhost", "https://fullweb.dev")
+        ),
+      },
     },
   },
-});
+  {
+    arrayMerge: (destinationArray, sourceArray, options) => sourceArray,
+  }
+);
