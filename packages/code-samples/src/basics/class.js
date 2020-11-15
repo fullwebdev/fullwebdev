f5fa81bf236b49fd69919f3f361418b46d0270c3
Noel Mace <contact@noelmace.com>
2020-11-15 14:47:43 +0100

    feat(code-samples): JS class vs prototype

---
+++ packages/code-samples/src/basics/class.js
@@ -0,0 +1,12 @@
+class Square {
+  // Constructeur avec définition des attributs
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  // Définition d'une méthode
+  grow() {
+    this.width++;
+  }
+}
