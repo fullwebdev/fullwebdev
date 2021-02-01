550988855019591bca014d5ec2894996484079a1
Noel Mace <contact@noelmace.com>
2021-02-01 16:22:41 +0100

    build(html-loader): clean project
    remove unecessary files and dependencies

--- packages/daucus/html-loader/test-web/html-loader.test.js
+++ packages/daucus/html-loader/test-web/html-loader.test.js
@@ -1,36 +1,36 @@
-import { html, fixture, expect, elementUpdated } from '@open-wc/testing';
-import '../html-loader.js';
-import { stub, assert } from 'sinon';
+import { html, fixture, expect, elementUpdated } from "@open-wc/testing";
+import "../html-loader.js";
+import { stub, assert } from "sinon";
 
-describe('HtmlLoader', () => {
+describe("HtmlLoader", () => {
   const mockTemplate = {
-    url: './template.html',
-    content: '<p>mock</p>',
+    url: "./template.html",
+    content: "<p>mock</p>",
   };
 
   beforeEach(() => {
-    const fetchStub = stub(window, 'fetch');
+    const fetchStub = stub(window, "fetch");
 
     fetchStub.withArgs(mockTemplate.url).returns(
       Promise.resolve(
         new Response(mockTemplate.content, {
           status: 200,
-          headers: { 'Content-type': 'text/html' },
+          headers: { "Content-type": "text/html" },
         })
       )
     );
 
     fetchStub.returns(
       Promise.resolve(
-        new Response('', {
+        new Response("", {
           status: 404,
-          headers: { 'Content-type': 'text/plain' },
+          headers: { "Content-type": "text/plain" },
         })
       )
     );
   });
 
-  it('import and render given template', async () => {
+  it("import and render given template", async () => {
     const el = await fixture(
       html`<html-loader href=${mockTemplate.url}></html-loader>`
     );
@@ -41,13 +41,13 @@ describe('HtmlLoader', () => {
     expect(el.innerHTML).to.equal(mockTemplate.content);
   });
 
-  it('only load template after href attribute has been set', async () => {
+  it("only load template after href attribute has been set", async () => {
     /** @type {HTMLLoaderElement} */
     const el = await fixture(html`<html-loader></html-loader>`);
 
     assert.notCalled(window.fetch);
     await elementUpdated(el);
-    expect(el.innerHTML).to.equal('');
+    expect(el.innerHTML).to.equal("");
 
     el.href = mockTemplate.url;
     await elementUpdated(el);
@@ -56,7 +56,7 @@ describe('HtmlLoader', () => {
     expect(el.innerHTML).to.equal(mockTemplate.content);
   });
 
-  it('load fallback if fetching href failed', async () => {
+  it("load fallback if fetching href failed", async () => {
     const el = await fixture(
       html`<html-loader
         href="./nope.html"
@@ -65,36 +65,36 @@ describe('HtmlLoader', () => {
     );
 
     assert.callCount(window.fetch, 2);
-    assert.calledWith(window.fetch, './nope.html');
+    assert.calledWith(window.fetch, "./nope.html");
     assert.calledWith(window.fetch, mockTemplate.url);
     await elementUpdated(el);
     expect(el.innerHTML).to.equal(mockTemplate.content);
   });
 
-  it('render error message if fetching href failed and no fallback was available', async () => {
+  it("render error message if fetching href failed and no fallback was available", async () => {
     const el = await fixture(
       html`<html-loader href="./nope.html"></html-loader>`
     );
 
-    assert.calledWith(window.fetch, './nope.html');
+    assert.calledWith(window.fetch, "./nope.html");
     await elementUpdated(el);
-    expect(el.firstChild).to.have.class('html-loader__error');
-    expect(el.firstChild.textContent).equals('(./nope.html) 404');
+    expect(el.firstChild).to.have.class("html-loader__error");
+    expect(el.firstChild.textContent).equals("(./nope.html) 404");
   });
 
-  it('cache templates', async () => {
+  it("cache templates", async () => {
     const el = await fixture(
       html`<html-loader href=${mockTemplate.url}></html-loader>`
     );
     await elementUpdated(el);
-    el.href = './nope.html';
+    el.href = "./nope.html";
     await elementUpdated(el);
     el.href = mockTemplate.url;
 
     assert.calledTwice(window.fetch);
     assert.callOrder(
       window.fetch.withArgs(mockTemplate.url),
-      window.fetch.withArgs('./nope.html')
+      window.fetch.withArgs("./nope.html")
     );
     await elementUpdated(el);
     expect(el.innerHTML).to.equal(mockTemplate.content);
