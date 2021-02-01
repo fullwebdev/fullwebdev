d6b104164247fa973a734eda17997a16b82dfbbf
Noel Mace <contact@noelmace.com>
2021-02-01 16:13:29 +0100

    build(daucus): reduce dev dependencies
    move test tools to the daucus root directory

---
+++ packages/daucus/web-test-runner.config.mjs
@@ -0,0 +1,20 @@
+import { playwrightLauncher } from "@web/test-runner-playwright";
+
+/** @type {import("@web/test-runner").TestRunnerConfig} */
+export default {
+  files: "*/test-web/**/*.test.{js,ts,html}",
+  nodeResolve: true,
+
+  /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
+  // esbuildTarget: 'auto',
+
+  /** Confgure bare import resolve plugin */
+  nodeResolve: true,
+
+  /** Browsers to run tests on */
+  browsers: [
+    playwrightLauncher({ product: "chromium" }),
+    playwrightLauncher({ product: "firefox" }),
+    // playwrightLauncher({ product: 'webkit' }),
+  ],
+};
