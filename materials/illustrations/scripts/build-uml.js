0083119d8ab902c2febb4642ea9e368bb75b75ad
Noel Mace <contact@noelmace.com>
2021-02-04 19:25:25 +0100

    chore(illustrations): apply eslint rules to scripts

--- materials/illustrations/scripts/build-uml.js
+++ materials/illustrations/scripts/build-uml.js
@@ -5,22 +5,22 @@ const path = require("path");
 /**
  * @param {string} path
  */
-(async srcdir => {
+(async (srcdir) => {
   const files = await fs.promises.readdir(srcdir);
   let count = 0;
   for await (const file of files) {
     const src = path.join(srcdir, file);
     const stat = await fs.promises.stat(src);
     if (stat.isFile() && src.match(/\.(pu|puml|plantuml)$/)) {
-      const dist = path.join(srcdir, "images", path.parse(src).name + ".svg");
+      const dist = path.join(srcdir, "images", `${path.parse(src).name}.svg`);
       console.log(`${src} → ${dist}`);
       const gen = plantuml.generate(src, { format: "svg" });
       const outStream = fs.createWriteStream(dist);
       gen.out.pipe(outStream);
-      count++;
+      count += 1;
     }
   }
   return count;
-})("graphs/").then(count => {
+})("graphs/").then((count) => {
   console.log(`${count} plantuml files processed`);
 });
