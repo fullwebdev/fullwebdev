527f66085d83cf845c8321ab4a8ce9779ce9c800
Noel Mace <contact@noelmace.com>
2021-02-06 15:03:07 +0100

    build: fix cp-assets

--- scripts/cp-assets.sh
+++ scripts/cp-assets.sh
@@ -3,7 +3,7 @@
 set -e +x
 
 rm -rf website/public/illustrations
-mkdir -p website/public/illustrations
+mkdir -p website/public/illustrations/graphs
 cp -r materials/illustrations/graphs/images website/public/illustrations/graphs/images
 cp -r materials/illustrations/images website/public/illustrations/images
 cp -r materials/illustrations/plots website/public/illustrations/plots
