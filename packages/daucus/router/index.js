27ed70f241fae5083a031bad58a7d4bbf93aec0f
Noel Mace <contact@noelmace.com>
2021-02-07 23:30:33 +0100

    chore(daucus-router): export programmatic router

--- packages/daucus/router/index.js
+++ packages/daucus/router/index.js
@@ -1,2 +1,3 @@
 export { DaucusRouter } from "./src/DaucusRouter.js";
 export { DaucusRouterOutlet } from "./src/DaucusRouterOutlet.js";
+export { createDaucusRouter } from "./src/create-router.js";
