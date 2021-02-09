9db409752f4d64e6726ed31643cf8f29c573786b
Noel Mace <contact@noelmace.com>
2021-02-09 16:28:22 +0100

    chore(helpers): wrong export of router

--- packages/helpers/index.js
+++ packages/helpers/index.js
@@ -1,3 +1,3 @@
 export { el } from "./el/index.js";
 export { Template, part } from "./template/index.js";
-export { Router } from "./router/index.js";
+export { AbstractRouter } from "./router/index.js";
