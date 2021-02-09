da2d51806f0879e4fe899d89ce0f574a4cff8f62
Noel Mace <contact@noelmace.com>
2021-02-09 16:37:40 +0100

    chore(website): refactor and update router

--- website/src/app/router.js
+++ website/src/app/router.js
@@ -1,4 +1,5 @@
-import { DaucusRouter } from "@daucus/router";
+import { AbstractRouter } from "@modern-helpers/router";
+import { routeFinder } from "@daucus/router";
 import daucusRoutes from "../fragments/routes.js";
 import "@daucus/html-loader/html-loader";
 
@@ -10,14 +11,6 @@ const preferredLanguages = window.navigator.languages || [
   window.navigator.language,
 ];
 
-/** @type {"fr" | "en"} */
-let language = "fr";
-if (!preferredLanguages.find((code) => code.startsWith("fr"))) {
-  language = "en";
-}
-
-/** @type {HTMLLoaderElement} */
-let outlet;
 /** @type {Node} */
 let homeTemplate;
 
@@ -26,65 +19,98 @@ const APP_ROUTES = {
   "/": {
     templateName: "homepage",
   },
+  "/404": {
+    templateName: "not-found",
+  },
 };
 
-class AppRouter extends DaucusRouter {
-  constructor() {
-    super(daucusRoutes, "/docs/en", "dist/fragments/");
+class AppRouter extends AbstractRouter {
+  /**
+   * @param {import("@daucus/router/src/RoutesConfig").RoutesConfig} daucusRoutesConfig
+   * @param {string} fragmentsDirectory
+   */
+  constructor(daucusRoutesConfig, fragmentsDirectory) {
+    super();
+    // FIXME: i18n
+    this._findDaucusRoute = routeFinder(daucusRoutesConfig);
+    this._fragmentsDirectory = fragmentsDirectory;
+
+    /** @type {"fr" | "en"} */
+    this.preferredLanguage = "fr";
+    if (!preferredLanguages.find((code) => code.startsWith("fr"))) {
+      this.preferredLanguage = "en";
+    }
+
+    /** @type {HTMLLoaderElement | null} */
+    this._outlet = null;
+  }
+
+  /** @type {HTMLLoaderElement} */
+  get outlet() {
+    if (!this._outlet) {
+      const outlet = document.querySelector("html-loader");
+      if (!outlet) throw new Error("no html-loader element could be found");
+      // @ts-ignore cast outlet from Element to HTMLLoaderElement
+      this._outlet = outlet;
+    }
+    // @ts-ignore this._outlet can't be null here
+    return this._outlet;
   }
 
   /**
    * @param {string} path
    * @param {NavigationOptions} [options]
+   *
+   * @returns {[path: string, options?: NavigationOptions] | null}
    */
-  async renderOrRedirect(path, options) {
+  renderOrRedirect(path, options) {
     const appRoute = APP_ROUTES[path];
+    /** @type {string | null} */
+    let templateUrl = null;
     if (appRoute) {
       if (appRoute.template) {
-        outlet.innerHTML = "";
-        outlet.appendChild(appRoute.template());
+        this.outlet.staticContent(appRoute.template());
+        return null;
         // TODO: lit-html & wc
-      } else if (appRoute.templateName) {
-        this.dispatchEvent(
-          new CustomEvent("route-match", {
-            detail: {
-              templateHRef: `/dist/fragments/app/${language}/${appRoute.templateName}.html`,
-            },
-          })
-        );
       }
+      if (appRoute.templateName) {
+        templateUrl = `${this._fragmentsDirectory}app/${this.preferredLanguage}/${appRoute.templateName}.html`;
+      }
+    } else {
+      const [daucusProjectName, daucusRoute] = this._findDaucusRoute(path);
+      if (daucusRoute && daucusRoute.templateUrl) {
+        templateUrl = `${this.base || "/"}${
+          this._fragmentsDirectory
+        }${daucusProjectName}/${this.preferredLanguage}/${
+          daucusRoute.templateUrl
+        }`;
+      }
+    }
+
+    if (templateUrl !== null) {
+      this.outlet.href = templateUrl;
       return null;
     }
-    return super.renderOrRedirect(path, options);
+
+    return ["/404", options];
   }
 
   // eslint-disable-next-line class-methods-use-this
-  initOutlet() {
+  resetPageContainer() {
     const pageContainer = document.getElementById("page-container");
-    if (!pageContainer) throw new Error("no page-container");
+    if (!pageContainer)
+      throw new Error("no html-loader nor page-container element found");
 
-    if (
-      pageContainer.firstElementChild &&
-      pageContainer.firstElementChild.id === "homepage"
-    ) {
+    if (pageContainer.firstElementChild) {
       homeTemplate = pageContainer.firstElementChild.cloneNode(true);
       APP_ROUTES["/"].template = () => homeTemplate.cloneNode(true);
-      // FIXME: for root index with i18n
     }
-    // @ts-ignore HTMLElement as HTMLLoaderElement
-    outlet = document.createElement("html-loader");
-    // TODO: outlet fallback
+    const outlet = document.createElement("html-loader");
     pageContainer.innerHTML = "";
     pageContainer.appendChild(outlet);
+    // @ts-ignore cast outlet from Element to HTMLLoaderElement
+    this._outlet = outlet;
   }
 }
 
-const router = new AppRouter();
-// @ts-ignore cast Event to RouteMatchEvent
-router.addEventListener("route-match", (/** @type {RouteMatchEvent} */ e) => {
-  if (e.detail.templateHRef) {
-    outlet.href = e.detail.templateHRef;
-  }
-});
-
-export { router };
+export const router = new AppRouter(daucusRoutes, "dist/fragments/");
