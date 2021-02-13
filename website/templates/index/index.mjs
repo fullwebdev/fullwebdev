9f7c2f6a88d15108ef6d333eb4ce0c2cab57c1c7
Noel Mace <contact@noelmace.com>
2021-02-13 23:53:33 +0100

    chore(website): footer & fixed header

--- website/templates/index/index.mjs
+++ website/templates/index/index.mjs
@@ -16,6 +16,11 @@ const nav = {
   fr: ["Apprendre", "Créer", "S'informer"],
 };
 
+const languageSwitcher = {
+  fr: 'This website is also available in <a href="" data-lang="en" aria-label="switch to english">English</a>.',
+  en: 'Ce site et des contenus supplémentaires sont également disponibles en <a href="" data-lang="fr" aria-label="passer en français">français</a>.'
+}
+
 export default {
   template: readTemplate(import.meta),
   files: {
@@ -23,6 +28,9 @@ export default {
       meta,
       nav: nav.en,
       home: Mustache.render(homepage.template, homepage.langs.en),
+      language: {
+        switch: languageSwitcher.en
+      }
     },
     "public/localized-files/fr_ALL/index.html": {
       meta: {
@@ -35,13 +43,18 @@ export default {
       },
       nav: nav.fr,
       home: Mustache.render(homepage.template, homepage.langs.fr),
+      language: {
+        switch: languageSwitcher.fr
+      }
     },
     "public/index.html": {
       meta,
       root: true,
       script: {
         nav: JSON.stringify(nav),
+        languageSwitch: JSON.stringify(languageSwitcher)
       },
+      language: {}
     },
   },
 };
