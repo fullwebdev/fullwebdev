d7a13728660f88116ef3bf9d9f754bfc9b503621
Noel Mace <contact@noelmace.com>
2021-02-15 14:39:57 +0100

    style: prettier

--- website/templates/index/index.mjs
+++ website/templates/index/index.mjs
@@ -17,9 +17,11 @@ const nav = {
 };
 
 const languageSwitcher = {
-  fr: 'This website is also available in <a href="" data-lang="en" aria-label="switch to english">English</a>.',
-  en: 'Ce site et des contenus supplémentaires sont également disponibles en <a href="" data-lang="fr" aria-label="passer en français">français</a>.'
-}
+  fr:
+    'This website is also available in <a href="" data-lang="en" aria-label="switch to english">English</a>.',
+  en:
+    'Ce site et des contenus supplémentaires sont également disponibles en <a href="" data-lang="fr" aria-label="passer en français">français</a>.',
+};
 
 export default {
   template: readTemplate(import.meta),
@@ -29,8 +31,8 @@ export default {
       nav: nav.en,
       home: Mustache.render(homepage.template, homepage.langs.en),
       language: {
-        switch: languageSwitcher.en
-      }
+        switch: languageSwitcher.en,
+      },
     },
     "public/localized-files/fr_ALL/index.html": {
       meta: {
@@ -44,17 +46,17 @@ export default {
       nav: nav.fr,
       home: Mustache.render(homepage.template, homepage.langs.fr),
       language: {
-        switch: languageSwitcher.fr
-      }
+        switch: languageSwitcher.fr,
+      },
     },
     "public/index.html": {
       meta,
       root: true,
       script: {
         nav: JSON.stringify(nav),
-        languageSwitch: JSON.stringify(languageSwitcher)
+        languageSwitch: JSON.stringify(languageSwitcher),
       },
-      language: {}
+      language: {},
     },
   },
 };
