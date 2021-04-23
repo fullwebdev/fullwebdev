d5f270ad557af15f758277b3e610e1c673d657a9
Noel Mace <contact@noelmace.com>
2021-04-24 01:45:31 +0200

    fix(daucus-cli): sort menu entries without prefix
    sort will now fallback to the route title, id or key if no prefix
    (i.e. position) was provided

--- packages/daucus/cli/src/routing/sort.js
+++ packages/daucus/cli/src/routing/sort.js
@@ -1,18 +1,33 @@
-/** @typedef {import('@daucus/core')} Route */
+/** @typedef {import('@daucus/core').Route} Route */
+
+/**
+ * @internal
+ * @param {[string, Partial<Route>]} routeChildEntry
+ */
+function getRouteEntryPosition(routeChildEntry) {
+  let position = "";
+  if (routeChildEntry && routeChildEntry[1]) {
+    const route = routeChildEntry[1];
+    if (route.position) {
+      position = route.position;
+    } else if (route.title) {
+      position = route.title;
+    } else if (route.id) {
+      position = route.id;
+    } else if (routeChildEntry[0]) {
+      [position] = routeChildEntry;
+    }
+  }
+  return position.padEnd(10, "~");
+}
 
 /**
  * @param {[string, Partial<Route>]} entryA
  * @param {[string, Partial<Route>]} entryB
  */
- export function sortRoutesChildEntriesByPosition(entryA, entryB) {
-  const positionA = (entryA && entryA[1] && entryA[1].position
-    ? entryA[1].position
-    : ""
-  ).padEnd(10, "~");
-  const positionB = (entryB && entryB[1] && entryB[1].position
-    ? entryB[1].position
-    : ""
-  ).padEnd(10, "~");
+export function sortRoutesChildEntriesByPosition(entryA, entryB) {
+  const positionA = getRouteEntryPosition(entryA);
+  const positionB = getRouteEntryPosition(entryB);
   if (positionA > positionB) return 1;
   if (positionA < positionB) return -1;
   return 0;
