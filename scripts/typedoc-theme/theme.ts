1913000519a6cc3643502a98b3f684a2c05f13f7
Noel Mace <contact@noelmace.com>
2021-04-24 02:12:05 +0200

    build: fix typedocs URLs with README and fragment

--- scripts/typedoc-theme/theme.ts
+++ scripts/typedoc-theme/theme.ts
@@ -17,19 +17,22 @@ class UtilsComponent extends ContextAwareRendererComponent {
     // override https://github.com/tgreyuk/typedoc-plugin-markdown/blob/0b9588b/packages/typedoc-plugin-markdown/src/components/options.ts#L49-L55
     MarkdownTheme.HANDLEBARS.registerHelper("relativeURL", (url: string) => {
       if (!url) return url;
-      const urlWithoutExt = url.replace(/(.*).md/, "$1");
-      if (!this.publicPath) return this.getRelativeUrl(urlWithoutExt);
+      const urlWithoutExt = url
+        .replace(/(.*)\.md/, "$1")
+        .replace(/^README/, "");
+      if (!this.publicPath) {
+        return this.getRelativeUrl(urlWithoutExt);
+      }
+      if (urlWithoutExt.startsWith("#")) {
+        return this.publicPath.replace(/\/$/, "") + urlWithoutExt;
+      }
       return this.publicPath + urlWithoutExt;
     });
   }
 }
 
 export default class GatsbyMarkdownTheme extends MarkdownTheme {
-  /**
-   * @param {import('typedoc').Renderer} renderer
-   * @param {string} basePath
-   */
-  constructor(renderer, basePath) {
+  constructor(renderer: import("typedoc").Renderer, basePath: string) {
     super(renderer, basePath);
 
     renderer.addComponent("my-theme-utils", new UtilsComponent(renderer));
