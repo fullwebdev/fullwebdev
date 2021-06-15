cb0960522aa45ab3f1f933c38ab8711a29e45255
Noel Mace <contact@noelmace.com>
2021-06-15 07:13:52 +0200

    chore(website): add img alt in footer

--- website/templates/index/index.mjs
+++ website/templates/index/index.mjs
@@ -8,6 +8,45 @@ const meta = {
   twitterCard: "https://fullweb.dev/images/social/social_en.png",
 };
 
+const copyrightTemplate = (props) => /* HTML */ `
+  <p>
+    Copyright &copy; 2018-${new Date().getFullYear()} Noël Macé, fullweb.dev
+  </p>
+  <div class="cc-license-icons">
+    <span class="cc-icon"
+      ><img
+        alt="cc logo"
+        src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" /></span
+    ><span class="cc-icon"
+      ><img
+        alt="cc attribution logo"
+        src="https://mirrors.creativecommons.org/presskit/icons/by.svg" /></span
+    ><span class="cc-icon"
+      ><img
+        alt="cc non commercial logo"
+        src="https://mirrors.creativecommons.org/presskit/icons/nc-eu.svg" /></span
+    ><span class="cc-icon"
+      ><img
+        alt="cc share alike logo"
+        src="https://mirrors.creativecommons.org/presskit/icons/sa.svg"
+    /></span>
+  </div>
+  <p>
+    ${props.license
+      .map((elmt) => {
+        if (elmt.href) {
+          return /* HTML */ `
+            <a href="${elmt.href}" target="_blank" rel="noopener noreferrer"
+              >${elmt.text}</a
+            >
+          `;
+        }
+        return elmt;
+      })
+      .join("")}
+  </p>
+`;
+
 const scriptWordings = {
   en: {
     navLinks: ["Resources", "Tools", "Blog", "About"],
@@ -15,7 +54,17 @@ const scriptWordings = {
       'Ce site et des contenus supplémentaires sont également disponibles en <a href="" data-lang="fr" aria-label="passer en français">français</a>.',
     editButton: "edit",
     languageSwitcherLabel: `Passer le site de l'anglais au français`,
-    copyright: `<p>Copyright &copy; 2018-${new Date().getFullYear()} Noël Macé, fullweb.dev</p><div class="cc-license-icons"><span class="cc-icon"><img alt="cc logo" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"></span><span class="cc-icon"><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg"></span><span class="cc-icon"><img src="https://mirrors.creativecommons.org/presskit/icons/nc-eu.svg"></span><span class="cc-icon"><img src="https://mirrors.creativecommons.org/presskit/icons/sa.svg"></span></div><p>Except where otherwise noted, content of this site is licensed under a <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank" rel="noopener noreferrer">Creative Common Attribution-NonCommercial-ShareAlike 4.0 International</a> license.</p>`,
+    copyright: copyrightTemplate({
+      license: [
+        "Except where otherwise noted, content of this site is licensed under a ",
+        {
+          href: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+          text:
+            "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)",
+        },
+        "license.",
+      ],
+    }),
   },
   fr: {
     navLinks: ["Ressources", "Outils", "Blog", "À propos"],
@@ -23,7 +72,17 @@ const scriptWordings = {
       'This website is also available in <a href="" data-lang="en" aria-label="switch to english">English</a>.',
     editButton: "éditer",
     languageSwitcherLabel: "Switch from french to English",
-    copyright: `<p>Copyright &copy; 2018-${new Date().getFullYear()} Noël Macé, fullweb.dev</p><div class="cc-license-icons"><span class="cc-icon"><img alt="cc logo" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"></span><span class="cc-icon"><img src="https://mirrors.creativecommons.org/presskit/icons/by.svg"></span><span class="cc-icon"><img src="https://mirrors.creativecommons.org/presskit/icons/nc-eu.svg"></span><span class="cc-icon"><img src="https://mirrors.creativecommons.org/presskit/icons/sa.svg"></span></div><p>Sauf mention contraire, le contenu de ce site est distribué sous licence <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.fr" target="_blank" rel="noopener noreferrer">Creative Common Attribution-NonCommercial-ShareAlike 4.0 International</a> </p>`,
+    copyright: copyrightTemplate({
+      license: [
+        "Sauf mention contraire, le contenu de ce site est distribué sous licence ",
+        {
+          href: "https://creativecommons.org/licenses/by-nc-sa/4.0/deed.fr",
+          text:
+            "Creative Commons Attribution - Pas d’Utilisation Commerciale - Partage dans les Mêmes Conditions 4.0 International (CC BY-NC-SA 4.0)",
+        },
+        ".",
+      ],
+    }),
   },
 };
 
