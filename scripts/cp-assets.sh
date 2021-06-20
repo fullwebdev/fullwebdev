39d4dee85347ad9e4aa83dbe498fe6dd97c6ad09
Noel Mace <contact@noelmace.com>
2021-06-20 02:56:09 +0200

    chore(illustrations): init browsers-history

--- scripts/cp-assets.sh
+++ scripts/cp-assets.sh
@@ -7,3 +7,4 @@ mkdir -p website/public/illustrations/graphs
 cp -r materials/illustrations/graphs/images website/public/illustrations/graphs/images
 cp -r materials/illustrations/images website/public/illustrations/images
 cp -r materials/illustrations/plots website/public/illustrations/plots
+cp -r materials/illustrations/browsers-history website/public/illustrations/browsers-history
