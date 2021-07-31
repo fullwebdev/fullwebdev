54e62d312555abf4897a119fb0521a60936efd8a
Noel Mace <contact@noelmace.com>
2021-07-31 19:58:37 +0200

    build: update materials assets location

--- scripts/cp-assets.sh
+++ scripts/cp-assets.sh
@@ -3,9 +3,11 @@
 set -e +x
 
 rm -rf website/public/illustrations
+rm -rf website/public/browsers-history
 mkdir -p website/public/illustrations/graphs
+mkdir -p website/public/browsers-history
 cp -r materials/illustrations/graphs/images website/public/illustrations/graphs/images
 cp -r materials/illustrations/images website/public/illustrations/images
 cp -r materials/illustrations/plots website/public/illustrations/plots
 cp -r materials/illustrations/browsers-history website/public/illustrations/browsers-history
-cp -r materials/browsers-history/assets website/public/browsers-history
+cp -r materials/browsers-history/assets/* website/public/browsers-history
