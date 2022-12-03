f5dee1930f33893b0140b89ce88383084d3a8fa1
Noel Mace <contact@noelmace.com>
2022-12-03 03:23:10 +0100

    fix(daucus/typedoc-theme): typedoc 0.23 upgrade
    add an index to actually register the theme and allow to use it
    outside of the fullwebdev monorepo
    upgrade typedoc to v0.23 and change code accordingly

---
+++ packages/daucus/typedoc-theme/src/index.ts
@@ -0,0 +1,7 @@
+import { Application } from 'typedoc';
+// eslint-disable-next-line import/extensions, import/no-unresolved
+import { DaucusMarkdownTheme } from './theme';
+
+export function load(app: Application) {
+  app.renderer.defineTheme('daucus', DaucusMarkdownTheme);
+}
