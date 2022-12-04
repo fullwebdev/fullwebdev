3e4809e40e11e3083b6a1089e56e1e175df3a9fd
Noel Mace <contact@noelmace.com>
2022-12-04 14:20:57 +0100

    build(website): fix dev server
    fix "Unscannable package import found" errors by adding the associated
    dependencies to Snowpack packageOptions.knownEntrypoints
    fix a typo in npm scripts and reorganise them so we can run the dev
    server without rerunning prepare-build
    
    see https://www.snowpack.dev/reference/configuration#packageoptionsknownentrypoints

--- website/snowpack.config.js
+++ website/snowpack.config.js
@@ -20,6 +20,17 @@ module.exports = {
       // dest: "/localized-files/en_ALL/index.html",
     },
   ],
+  packageOptions: {
+    knownEntrypoints: [
+      "lit-html",
+      "lit-html/is-server.js",
+      "lit-html/directives/if-defined.js",
+      "lit-html/directives/class-map.js",
+      "lit-html/directives/style-map.js",
+      "lit-html/directives/unsafe-html.js",
+      "lit-html/directives/live.js",
+    ],
+  },
   devOptions: {
     open: "none",
   },
